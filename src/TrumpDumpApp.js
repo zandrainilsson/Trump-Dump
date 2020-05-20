030cb2e61d8a3d950794c7ddbddef84ca5171972
Zandra <zandra.nilsson93@gmail.com>
2020-05-20 14:10:55 +0200

    Satt upp react app

---
+++ src/TrumpDumpApp.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class TrumpDumpApp extends Component {
+    render() {
+        return <h1>Trump Dump</h1>
+    }
+}
+
+export default TrumpDumpApp;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TrumpDumpApp from './TrumpDumpApp.js';
+
+ReactDOM.render(<TrumpDumpApp />,document.getElementById('root'));
+
+
